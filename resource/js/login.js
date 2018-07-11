48b47098f1e62c26c4ee9826e6aedffec09ce659
MomoKiller <1226424558@qq.com>
2018-07-12 00:59:21 +0800

    finash web login page

--- resource/js/login.js
+++ resource/js/login.js
@@ -4,7 +4,34 @@ $(function(){
 
         },
         init: function(){
+            login.login();
+            login.toRegister();
+            login.register();
+            login.toLogin();
+        },
+        // 登录
+        login: function () {
 
+        },
+        // 去注册
+        toRegister: function(){
+            $('.to_register').bind('click',function(){
+                $('.login_box').addClass('box_hidden');
+                $('.register_box').removeClass('box_hidden');
+            });
+        },
+        // 注册
+        register: function(){
+
+        },
+        // 去登录
+        toLogin: function(){
+            $('.to_login').bind('click',function(){
+                $('.register_box').addClass('box_hidden');
+                $('.login_box').removeClass('box_hidden');
+            });
         }
-    }
+    };
+    // 初始化
+    login.init();
 });
