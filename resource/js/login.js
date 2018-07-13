a7e763c992b35d15e80fe5356da1d54134180912
MomoKiller <1226421458@qq.com>
2018-07-13 15:40:44 +0800

    下拉框，弹框 0.0

--- resource/js/login.js
+++ resource/js/login.js
@@ -8,6 +8,7 @@ $(function(){
             login.toRegister();
             login.register();
             login.toLogin();
+            login.cityCelect();
         },
         // 登录
         login: function () {
@@ -34,6 +35,49 @@ $(function(){
                 $('.register_box').addClass('box_hidden');
                 $('.login_box').removeClass('box_hidden');
             });
+        },
+        cityCelect: function () {
+            var d = [{
+                key: '上海',
+                value: '上海'
+            },{
+                key: '北京',
+                value: '北京'
+            }];
+            login.dropBox('address_wrap', d);
+        },
+        // 下拉框
+        dropBox: function(domId, data) {
+            var dropHtml = '<div class="select_box city_select"><ul>';
+            for(var i=0; i< data.length; i++){
+                dropHtml += '<li><option value="'+ data[i].key+'">'+ data[i].value +'</option></li>';
+            }
+            dropHtml += '</ul></div>';
+            $('#'+ domId).addClass('posit_relative');
+            $('#'+ domId +' input').after(dropHtml);
+            $('#'+ domId +' input').bind('click', function () {
+                openBox();
+            });
+            $('#'+ domId + ' a').bind('click', function () {
+                if(!$(this).hasClass('drop_back')){
+                    openBox();
+                }else {
+                    closeBox();
+                }
+            });
+            $('.select_box ul li option').bind('click', function () {
+                var cityVal = $(this).val();
+                $('#'+ domId +' input').val(cityVal);
+                closeBox();
+            });
+            function openBox() {
+                $('.select_box').css('display', 'block');
+                $('#'+ domId+ ' a').addClass('drop_back');
+            }
+            function closeBox() {
+                $('.select_box').css('display', 'none');
+                $('#'+ domId+ ' a').removeClass('drop_back');
+            }
         }
     };
     // 初始化
