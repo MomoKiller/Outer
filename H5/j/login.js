7edbe95477156aa317f8c1727020e673859a20cb
MomoKiller <1226424558@qq.com>
2018-07-15 22:01:33 +0800

    样式小改动

--- H5/j/login.js
+++ H5/j/login.js
@@ -1,3 +1,21 @@
 /**
  * Created by pkk on 2018/7/14.
  */
+$(function(){
+    var login = {
+        api:{
+            'login': '',
+            'register': ''
+        },
+        init: function(){
+            login.toRegister();
+        },
+        toRegister: function(){
+            $('.login_box .to_register').bind('click', function(){
+                $('.login_box').addClass('box_hidden');
+                $('.register_box').removeClass('box_hidden');
+            });
+        }
+    };
+    login.init();
+});
