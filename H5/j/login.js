8189a96d6bdaf6503090ebbd8253fbaa95f25a11
MomoKiller <1226421458@qq.com>
2018-07-16 14:24:21 +0800

    s

--- H5/j/login.js
+++ H5/j/login.js
@@ -9,11 +9,18 @@ $(function(){
         },
         init: function(){
             login.toRegister();
+            login.toLogin();
         },
         toRegister: function(){
             $('.login_box .to_register').bind('click', function(){
                 $('.login_box').addClass('box_hidden');
                 $('.register_box').removeClass('box_hidden');
+            });
+        },
+        toLogin: function () {
+            $('.register_box .to_login').bind('click', function () {
+                $('.register_box').addClass('box_hidden')
+                $('.login_box').removeClass('box_hidden');
             });
         }
     };
